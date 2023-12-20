94adf71cd16de769c9f762477af585a16fcba2de
Carlos77-code <carloslimafernandes@hotmail.com>
2023-12-19 22:23:41 -0300

    Criada variavel "driver"
    Criando a primeira interação com o navegador

--- SiteInvestidorDez/UnitTest1.cs
+++ SiteInvestidorDez/UnitTest1.cs
@@ -1,18 +1,30 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SiteInvestidorDez
 {
     public class Tests
     {
+        public IWebDriver driver;
+
         [SetUp]
-        public void Setup()
+        public void InicioTeste()
         {
         }
 
         [Test]
-        public void Test1()
+        public void Teste()
         {
-            Assert.Pass();
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://investidor10.com.br/");
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void FimDoTeste()
+        {
+
         }
     }
 }
